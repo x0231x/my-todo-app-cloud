eb9d7a22e6ee62181044afea1160a131c8de4920
x0231x <g534894123@gmail.com>
2025-06-18 22:02:56 +0800

    Settings.py新增REST_FRAMEWORK AllowAny

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -131,3 +131,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
